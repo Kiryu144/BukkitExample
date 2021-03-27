97da6a4b28856ed810f4959ea353beea606be4fc
Kiryu144 <123davidLP@gmail.com>
2021-03-27 17:30:23 +0100

    Initial commit

---
+++ src/main/java/at/dklostermann/bukkit/example/Example.java
@@ -0,0 +1,18 @@
+package at.dklostermann.bukkit.example;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Example extends JavaPlugin
+{
+    @Override
+    public void onEnable()
+    {
+
+    }
+
+    @Override
+    public void onDisable()
+    {
+
+    }
+}
